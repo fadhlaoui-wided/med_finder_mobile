170260430ef6014b03ab47f47b8d48a3e3441651
waj949 <wajdihamrouni1993@gmail.com>
2020-02-15 15:47:05 +0100

    auth

--- components/login.jsx
+++ components/login.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
 import * as axios from 'axios';
-
+import { AuthSession } from 'expo';
 
 
 export default class Login extends Component {
@@ -10,10 +10,27 @@ export default class Login extends Component {
     
     this.state = {
       email:'',
-      password:''
+      password:'',
+      result: null
     };
   }
-  
+  _handlePressAsync = async () => {
+    let redirectUrl = AuthSession.getRedirectUrl(); 
+    let result = await AuthSession.startAsync(
+       {
+        
+        authUrl:
+          `https://www.facebook.com/v2.8/dialog/oauth?response_type=token`  +
+          `&client_id=${FB_APP_ID}` +
+          `&redirect_uri=${
+            encodeURIComponent(
+              redirectUrl
+            ) }`,
+      }
+    );
+    this.setState({ result });
+  };
+
   onLogin() {
     const { email, password } = this.state;
     const config = {
@@ -36,9 +53,12 @@ export default class Login extends Component {
    this.props.navigation.navigate('signup')
   }
 
+  
   render() {
     return (
       <View style={styles.container}>
+        <Button title="Open FB Auth" onPress={this._handlePressAsync} />
+        {this.state.result ? <Text>{JSON.stringify(this.state.result)}</Text> : null}
           <TextInput
           value={this.state.email}
           onChangeText={(email) => this.setState({ email })}
